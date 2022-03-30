74febd31e1a797f3941a6276c78dbb67b639c931
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-03-30 22:14:25 +0300

    Update 04_post_comment_polarity.py

--- source_code/04_post_comment_polarity.py
+++ source_code/04_post_comment_polarity.py
@@ -2,7 +2,7 @@ import json
 from textblob import TextBlob
 from matplotlib import pyplot as plt
 
-file = "../Lab04_subjectivity_polarity/collected_data_old/posts.json"
+file = "/collected_data_old/posts.json"
 print(file)
 comment_values = []
 post_values = []
